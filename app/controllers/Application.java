4b6cf2b9eb4293f49acca0161aa641a4f145ba3c
Al Johri <al.johri@gmail.com>
2013-05-19 09:44:35 -0500

    multiple methods for getting events

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -28,23 +28,59 @@ public class Application extends Controller {
 	}
 
 	public static Result events() {
-		String MY_ACCESS_TOKEN = "CAACEdEose0cBAOBMsL2Cz1dzKqQv1QBA1zE5vl6wFziPMBZA4eKKSJCvJnzdkCEZBC8nlgacUt95XSDYtdhcVUiC76Xnpo6ea64KqiMeyUuXPp34EBh683JBfjIBO44nef0YK3NnjsyxlA2pLVT7y797AMRVwZD";
+		String MY_ACCESS_TOKEN = "CAADw2gpVHk4BACSRNR9s5S4GAHII343CVvI1Vm06SLw3pFwGsxalFGcJh96PmZA1LqiamklvhjMlBaeSEKsMGhUD0z1laAbbiI0xgERZB9tm3BUIPceu9OZAIGMuWwBRqYanc7Afc9NyEv0pvkKDYZBLZCEVSBYYZD";
 		FacebookClient facebookClient = new DefaultFacebookClient(MY_ACCESS_TOKEN);
 
 		// Loop through connection object : https://groups.google.com/d/msg/restfb/eHMSgUxEXi4/gemE6_meNyAJ
+		
+		// Facebook Graph API (Method 1)
+
 		Connection<JsonObject> connection = facebookClient.fetchConnection( 
 			"search", JsonObject.class, 
 			Parameter.with("q", "*"), 
 			Parameter.with("center", "42.052925,-87.665834"), 
 			Parameter.with("type", "event"),
-			Parameter.with("distance", 1000)
+			Parameter.with("distance", 10000),
+			Parameter.with("fields", "id,owner,picture,description,location,venue,start_time,end_time")
+			// Parameter.with("limit", 100)
 		);
 
 		JsonArray events = new JsonArray();
+
+		JsonArray method1_events = new JsonArray();
 		do {
-			events.put(connection.getData());
+			//events.put(connection.getData());
+  			for (JsonObject jsonObject : connection.getData()) { 
+  				events.put(jsonObject);
+  				method1_events.put(jsonObject);
+  			}
 			connection = facebookClient.fetchConnectionPage(connection.getNextPageUrl(), JsonObject.class);
 		} while (connection.hasNext());
+		
+		System.out.println("Method 1");
+		System.out.println(method1_events.toString());
+
+		// FQL Query (Method 2)
+		
+		String fql_query = "SELECT eid, name, start_time, end_time, description, location, venue, pic, pic_big, pic_cover, parent_group_id FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid IN (SELECT page_id FROM place WHERE distance(latitude, longitude, '42.054774', '-87.67654') < 5000 LIMIT 51000) LIMIT 51000) AND venue.id IN (SELECT page_id FROM place WHERE distance(latitude,longitude, '42.054774', '-87.67654') < 5000 LIMIT 51000) ORDER BY start_time ASC LIMIT 51000";
+		List<JsonObject> list_events2 = facebookClient.executeFqlQuery(fql_query, JsonObject.class);
+
+		JsonArray method2_events = new JsonArray();
+		for (JsonObject jsonObject : list_events2) {
+			events.put(jsonObject);
+			method2_events.put(jsonObject);
+		}
+
+		System.out.println("Method 2:");
+		System.out.println(method2_events.toString());		
+
+		// Method 3
+		
+		// lalala
+		// lalalla
+		
+
+		// Remove Duplicates
 
 		System.out.println(events.toString());
 
