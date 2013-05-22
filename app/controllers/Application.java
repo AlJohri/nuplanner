a6d1097b53587d56b48662803a9c8fe569f35f0b
Chunxiao <diaochunxiao@gmail.com>
2013-05-21 21:32:29 -0500

    removed method one

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -42,37 +42,37 @@ public class Application extends Controller {
 	// http://www.daniweb.com/software-development/java/threads/379327/how-to-use-linkedlist
 	
 	public static Result events() {
-		String MY_ACCESS_TOKEN = "CAACEdEose0cBAGOZCa7mJoEZCSPZBNn6geHlruiZAiwmv2BAYZB553DEMW7g5ZCJfjkZC3P1JxmI9HpT9GbiXIcQilCIOGb8fFDJYeZC53F0YxCtTpFLCwdLoF8jlQBE65xt8hccWKhJ0v9nrfSKaVOe60ZAImQ6YK5IZD";
+		String MY_ACCESS_TOKEN = "CAACEdEose0cBAOAj8v4rfLN2MEhdw8nxXISTaBzrRVzHmnMBc98ZC6N4UPiZATH75ZBFytCUHexSo1DdMft2ren0UsnbovCZChpZAr8u8PynP8wJwrzkf2GEokERdoR85hgdJvVsNrL17oC9mI8a16AbbPNXi3pvrqLW5AjmcBQZDZD";
 		FacebookClient facebookClient = new DefaultFacebookClient(MY_ACCESS_TOKEN);
 
 		// Loop through connection object : https://groups.google.com/d/msg/restfb/eHMSgUxEXi4/gemE6_meNyAJ
 		
 		// Facebook Graph API (Method 1)
 
-		Connection<JsonObject> connection = facebookClient.fetchConnection( 
-			"search", JsonObject.class, 
-			Parameter.with("q", "*"), 
-			Parameter.with("center", "42.052925,-87.665834"), 
-			Parameter.with("type", "event"),
-			Parameter.with("distance", 10000),
-			Parameter.with("fields", "id,owner,picture,description,location,venue,start_time,end_time")
-			// Parameter.with("limit", 100)
-		);
+		// Connection<JsonObject> connection = facebookClient.fetchConnection( 
+		// 	"search", JsonObject.class, 
+		// 	Parameter.with("q", "*"), 
+		// 	Parameter.with("center", "42.052925,-87.665834"), 
+		// 	Parameter.with("type", "event"),
+		// 	Parameter.with("distance", 10000),
+		// 	Parameter.with("fields", "id,owner,picture,description,location,venue,start_time,end_time")
+		// 	// Parameter.with("limit", 100)
+		// );
+
+		// JsonArray method1_events = new JsonArray();
+		// do {
+		// 	//events.put(connection.getData());
+  // 			for (JsonObject jsonObject : connection.getData()) { 
+  // 				events.put(jsonObject);
+  // 				method1_events.put(jsonObject);
+  // 			}
+		// 	connection = facebookClient.fetchConnectionPage(connection.getNextPageUrl(), JsonObject.class);
+		// } while (connection.hasNext());
+		
+		// System.out.println("Method 1");
+		// System.out.println(method1_events.toString());
 
 		JsonArray events = new JsonArray();  // Change THIS variable to a LinkedList
-
-		JsonArray method1_events = new JsonArray();
-		do {
-			//events.put(connection.getData());
-  			for (JsonObject jsonObject : connection.getData()) { 
-  				events.put(jsonObject);
-  				method1_events.put(jsonObject);
-  			}
-			connection = facebookClient.fetchConnectionPage(connection.getNextPageUrl(), JsonObject.class);
-		} while (connection.hasNext());
-		
-		System.out.println("Method 1");
-		System.out.println(method1_events.toString());
 
 		// FQL Query (Method 2)
 		
