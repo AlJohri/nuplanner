a1fb578694e92fc93191bc04692b98a62e05f38b
sai <saikolli2012@u.northwestern.edu>
2013-05-27 17:02:33 -0500

    fixed merge conflicts

--- project/Build.scala
+++ project/Build.scala
@@ -13,20 +13,13 @@ object ApplicationBuild extends Build {
     javaJdbc,
     javaEbean,
     jdbc,
-<<<<<<< HEAD
+
     "org.jsoup" % "jsoup" % "1.7.2",
     "org.json" % "json" % "20090211",
     "com.google.code.gson" % "gson" % "2.2.4", 
     "postgresql" % "postgresql" % "9.1-901.jdbc4",
     "com.restfb" % "restfb" % "1.6.12",
     "com.gistlabs" % "mechanize" % "0.11.0",
-=======
-    "org.jsoup" % "jsoup" % "1.7.2", // doesn't have support for JSON, thus must use GSON
-    "com.google.code.gson" % "gson" % "2.2.4", // used for parsing JSON
-    "postgresql" % "postgresql" % "9.1-901.jdbc4", // postgres database for heroku
-    "com.restfb" % "restfb" % "1.6.12", // used for accessing facebook API (also has json support)
-    "com.gistlabs" % "mechanize" % "0.11.0", // unecessary if jsoup is being used (supports HTML, JSON, and XML)
->>>>>>> 3a43814901473d989550eff0f072a4059446ceb7
     "commons-io" % "commons-io" % "2.3"
   )
 
