ba2731786a1202d1088506fc6103c41cecab9fdb
Chunxiao <diaochunxiao@gmail.com>
2013-05-27 17:52:06 -0500

    add organization class

---
+++ app/models/MyOrganization.java
@@ -0,0 +1,48 @@
+package models;
+//json
+import org.json.*;
+
+import com.google.gson.*;
+//
+
+
+import java.util.*;
+import javax.persistence.*;
+
+// http://www.avaje.org/static/javadoc/pub/
+import play.db.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+import org.joda.time.DateTime;
+
+@Entity
+public class MyOrganization extends Model {
+    @Id public Long eid;
+    @Constraints.Required public String name;
+    @Constraints.Required public String creator;
+
+    public Long fbid;
+    public String url;
+
+    @Column(columnDefinition = "TEXT")
+    @Constraints.Required public String description;
+
+    public MyOrganization(Long fbid, String name,String creator, String url, String description) { 
+        this.fbid = fbid;
+        this.name = name;
+        this.creator = creator;
+        this.url = url;
+        this.description = description;
+    }
+    public String getJsonObject(){
+    	MyOrganization obj = new MyOrganization(this.fbid,this.name,this.creator,this.url,this.description);
+    	Gson gson = new Gson();
+    	String json = gson.toJson(obj);
+    	return json;
+    }
+    // http://www.avaje.org/static/javadoc/pub/com/avaje/ebean/ExpressionList.html
+    public static Finder<String,MyOrganization> find = new Finder<String,MyOrganization>( String.class, MyOrganization.class );
+    public static Finder<Long,MyOrganization> findLong = new Finder<Long,MyOrganization>( Long.class, MyOrganization.class );
+ 
+}
