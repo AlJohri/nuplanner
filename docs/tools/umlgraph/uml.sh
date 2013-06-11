6d81aa47afcc55dbf173f9a57d8e5b85a86a1bdb
Al Johri <al.johri@gmail.com>
2013-06-11 11:13:46 -0500

    Changed UML diagrams.

--- docs/tools/umlgraph/uml.sh
+++ docs/tools/umlgraph/uml.sh
@@ -4,20 +4,25 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 JARS="UmlGraph-5.6.6.jar"
 PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/lib/tools.jar:$JARS:"/usr/local/graphviz-2.14/bin"
 DOCLET=org.umlgraph.doclet.UmlGraph
-PACKAGES="models controllers.scrape controllers.seed controllers.utilities"
-export PATH DOCLET JARS PACKAGES
+export PATH DOCLET JARS
 
+PACKAGES="models"
+OUTPUT="models.png"
 java -classpath $PATH $DOCLET -sourcepath ../../../app $PACKAGES -attributes -operations -types
 perl -pi -e 's/<any>/any/g' graph.dot
-dot -Tpng -o ../../graph1.png graph.dot
-rm graph.dot
-cp ../../graph1.png ../../../public/graph1.png
+dot -Tpng -o ../../$OUTPUT graph.dot; rm graph.dot
+cp ../../$OUTPUT ../../../public/$OUTPUT
+
+PACKAGES="controllers.scrape controllers.seed controllers.utilities"
+OUTPUT="controller.png"
+java -classpath $PATH $DOCLET -sourcepath ../../../app $PACKAGES -attributes -operations -types
+perl -pi -e 's/<any>/any/g' graph.dot
+dot -Tpng -o ../../$OUTPUT graph.dot; rm graph.dot
+cp ../../$OUTPUT ../../../public/$OUTPUT
 
 PACKAGES="controllers.facebook"
+OUTPUT="facebook.png"
 java -classpath $PATH $DOCLET -sourcepath ../../../app $PACKAGES -attributes -operations -types
 perl -pi -e 's/<any>/any/g' graph.dot
-dot -Tpng -o ../../graph2.png graph.dot
-rm graph.dot
-cp ../../graph2.png ../../../public/graph2.png
-
-
+dot -Tpng -o ../../$OUTPUT graph.dot; rm graph.dot
+cp ../../$OUTPUT ../../../public/$OUTPUT
