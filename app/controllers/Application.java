b9dd35f2c9a18deba5f7811a8ec11e6bd1b9e090
Al Johri <al.johri@gmail.com>
2013-06-11 10:13:24 -0500

    Event handler now finds any overlapping events within date range.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,7 +13,7 @@ import org.json.*;
 import org.json.simple.JSONValue;
 import org.apache.commons.io.FileUtils;
 import org.joda.time.DateTime;
-import com.avaje.ebean.ExpressionList;
+import com.avaje.ebean.*;
 
 public class Application extends Controller {
 
@@ -32,27 +32,34 @@ public class Application extends Controller {
     public static Result events() {
         String start_string = request().getQueryString("start");
         String end_string = request().getQueryString("end");
-        String query = request().getQueryString("query");
+        String filter = request().getQueryString("query");
 
-        ExpressionList<MyEvent> events = MyEvent.find.where();
+        DateTime start, end;
+        /* if (start_string != null) */ start = new DateTime(Long.parseLong(start_string) * 1000);
+        /* if (end_string != null) */ end = new DateTime(Long.parseLong(end_string) * 1000);        
 
-        if (start_string != null) {
-            DateTime start = new DateTime(Long.parseLong(start_string) * 1000);
-            events = events.gt("start_time", start);
-        }
-        if (end_string != null) {
-            DateTime end = new DateTime(Long.parseLong(end_string) * 1000);
-            events = events.lt("start_time", end);
-        }
-        if (query != null) {
-            events = events.ilike("name", "%"+query+"%");
-        }
-
-        List<MyEvent> eventList = events.findList();
+        Query<MyEvent> query = Ebean.createQuery(MyEvent.class);
+        query.where( 
+            Expr.and (
+                Expr.and ( Expr.lt("start_time", end), Expr.gt("end_time", start) ),                
+                Expr.ilike("name", "%"+filter+"%")
+            )
+        );
+        
+        List<MyEvent> eventList = query.findList();
         eventList = (start_string == null && end_string == null) ? MyEvent.find.all() : eventList;
 
         return ok( JSONValue.toJSONString(eventList) );
 
+    }
+
+    /**
+     * Displays Google Doc of Final Report
+     * @return redirect to google doc
+     */
+    public static Result report() {
+        return redirect("https://docs.google.com/document/d/1j-DglRrHZORnoNX7j9KL7xZvRO86BFKDIQ4GNFKddkY");
+        //return redirect("https://docs.google.com/document/d/1j-DglRrHZORnoNX7j9KL7xZvRO86BFKDIQ4GNFKddkY/export?format=pdf");
     }
 
 }
